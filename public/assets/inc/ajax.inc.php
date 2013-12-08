81eb76c2e2320fd1f83cd940e6d46beac334b155
notatree <happynotatree@gmail.com>
2013-12-08 21:37:16 +0800

    do some cleaning

--- public/assets/inc/ajax.inc.php
+++ public/assets/inc/ajax.inc.php
@@ -1,68 +1,69 @@
 <?php
-/*
-* Created on 2012-5-14 by lunwei
-*/
 
 /*
-* Enable sessions
-*/
+ * Created on 2012-5-14 by lunwei
+ */
+
+/*
+ * Enable sessions
+ */
 session_start();
 
 /*
-* Include necessary files
-*/
+ * Include necessary files
+ */
 include_once '../../../sys/config/db-cred.inc.php';
 
 /*
-* Define constants for config info
-*/
-foreach ( $C as $name => $val ){
-  define($name, $val);
+ * Define constants for config info
+ */
+foreach ($C as $name => $val) {
+    define($name, $val);
 }
 
 $actions = array(
-  'login_form' => array(
-    'object' => 'Admin',
-    'method' => 'displayLoginForm'
-  ),
-  'event_view' => array(
-    'object' => 'Calendar',
-    'method' => 'displayEvent'
-  ),
-  'edit_event' => array(
-    'object' => 'Calendar',
-    'method' => 'displayForm'
-  ),
-  'event_edit' => array(
-    'object' => 'Calendar',
-    'method' => 'processAjaxForm'
-  ),
-  'delete_event' => array(
-    'object' => 'Calendar',
-    'method' => 'confirmDelete'
-  ),
-  'confirm_delete' => array(
-    'object' => 'Calendar',
-    'method' => 'confirmDelete'
-  )
+    'login_form' => array(
+        'object' => 'Admin',
+        'method' => 'displayLoginForm'
+    ),
+    'event_view' => array(
+        'object' => 'Calendar',
+        'method' => 'displayEvent'
+    ),
+    'edit_event' => array(
+        'object' => 'Calendar',
+        'method' => 'displayForm'
+    ),
+    'event_edit' => array(
+        'object' => 'Calendar',
+        'method' => 'processAjaxForm'
+    ),
+    'delete_event' => array(
+        'object' => 'Calendar',
+        'method' => 'confirmDelete'
+    ),
+    'confirm_delete' => array(
+        'object' => 'Calendar',
+        'method' => 'confirmDelete'
+    )
 );
 
-if ( isset($actions[$_POST['action']]) ){
-  $use_array = $actions[$_POST['action']];
-  $obj = @new $use_array['object']($dbo);
-  if ( isset($_POST['event_id']) ){
-    $id = (int) $_POST['event_id'];
-  }
-  else{
-    $id = NULL;
-  }
-  echo @$obj->$use_array['method']($id);
+if (isset($actions[$_POST['action']])) {
+    $use_array = $actions[$_POST['action']];
+    $obj = @new $use_array['object']($dbo);
+    if (isset($_POST['event_id'])) {
+        $id = (int) $_POST['event_id'];
+    } else {
+        $id = NULL;
+    }
+    echo @$obj->$use_array['method']($id);
 }
 
-function __autoload($class_name){
-  $filename= '../../../sys/class/class.'. strtolower($class_name) . '.inc.php';
-  if ( file_exists($filename) ){
-    include_once $filename;
-  }
+function __autoload($class_name) {
+    $filename = '../../../sys/class/class.' . strtolower($class_name) . '.inc.php';
+    if (file_exists($filename)) {
+        include_once $filename;
+    }
 }
+
 ?>
--- public/assets/inc/process.inc.php
+++ public/assets/inc/process.inc.php
@@ -1,64 +1,64 @@
 <?php
+
 /*
-* Created on 2012-5-14 by lunwei
-*/
+ * Created on 2012-5-14 by lunwei
+ */
 
 session_start();
 
 /*
-* Include necessary files
-*/
+ * Include necessary files
+ */
 include_once '../../../sys/config/db-cred.inc.php';
 
 /*
-* Define constants for config info
-*/
-foreach ( $C as $name => $val ){
-  define($name, $val);
+ * Define constants for config info
+ */
+foreach ($C as $name => $val) {
+    define($name, $val);
 }
 $actions = array(
-  'event_edit' => array(
-    'object' => 'Calendar',
-    'method' => 'processForm',
-    'header' => 'Location: ../../'
-  ),
-  'user_login' => array(
-    'object' => 'Admin',
-    'method' => 'processLoginForm',
-    'header' => 'Location: ../../'
-  ),
-  'user_logout' => array(
-    'object' => 'Admin',
-    'method' => 'processLogout',
-    'header' => 'Location: ../../'
-  )
+    'event_edit' => array(
+        'object' => 'Calendar',
+        'method' => 'processForm',
+        'header' => 'Location: ../../'
+    ),
+    'user_login' => array(
+        'object' => 'Admin',
+        'method' => 'processLoginForm',
+        'header' => 'Location: ../../'
+    ),
+    'user_logout' => array(
+        'object' => 'Admin',
+        'method' => 'processLogout',
+        'header' => 'Location: ../../'
+    )
 );
 
 /*
-* Make sure the anti-CSRF token was passed and that the
-* requested action exists in the lookup array
-*/
-if ( $_POST['token']==$_SESSION['token']&& isset($actions[$_POST['action']]) ){
-  $use_array = $actions[$_POST['action']];
-  $obj = @new $use_array['object']($dbo);
-  if (TRUE === $msg=$obj->$use_array['method']()){
-    header($use_array['header']);
+ * Make sure the anti-CSRF token was passed and that the
+ * requested action exists in the lookup array
+ */
+if ($_POST['token'] == $_SESSION['token'] && isset($actions[$_POST['action']])) {
+    $use_array = $actions[$_POST['action']];
+    $obj = @new $use_array['object']($dbo);
+    if (TRUE === $msg = $obj->$use_array['method']()) {
+        header($use_array['header']);
+        exit;
+    } else {
+        die($msg);
+    }
+} else {
+    // Redirect to the main index if the token/action is invalid
+    header("Location: ../../");
     exit;
-  }
-  else{
-    die ( $msg );
-  }
-}
-else{
-  // Redirect to the main index if the token/action is invalid
-  header("Location: ../../");
-  exit;
 }
 
-function __autoload($class_name){
-  $filename = '../../../sys/class/class.'.strtolower($class_name) . '.inc.php';
-  if ( file_exists($filename) ){
-    include_once $filename;
-  }
+function __autoload($class_name) {
+    $filename = '../../../sys/class/class.' . strtolower($class_name) . '.inc.php';
+    if (file_exists($filename)) {
+        include_once $filename;
+    }
 }
+
 ?>
